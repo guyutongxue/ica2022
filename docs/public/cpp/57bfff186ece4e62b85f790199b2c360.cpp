5a0ee9e174ed11443348b2aa2f369552bba70b36
Guyutongxue <guyutongxue@163.com>
2022-09-15 19:45:49 +0800

    add answer

---
+++ docs/public/cpp/57bfff186ece4e62b85f790199b2c360.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <utility>
+#include <array>
+
+template<int N>
+struct Fib {
+    static constexpr const int val{Fib<N-1>::val + Fib<N-2>::val};
+};
+template<>
+struct Fib<0> {
+    static constexpr const int val{0};
+};
+template<>
+struct Fib<1> {
+    static constexpr const int val{1};
+};
+
+template<int... Vals>
+constexpr std::array<int, sizeof...(Vals)> save_impl(std::integer_sequence<int, Vals...>) {
+    return { Fib<Vals>::val... };
+}
+template<int N>
+constexpr auto save() {
+    return save_impl(std::make_integer_sequence<int, N>{});
+}
+
+constexpr auto fib{save<21>()};
+
+int main() {
+    int n;
+    std::cin >> n;
+    while (n--) {
+        int i;
+        std::cin >> i;
+        std::cout << fib[i] << std::endl;
+    }
+}
