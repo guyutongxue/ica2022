ea2e18b2529de29ea6f3ebba5e0e22ce46eaf01b
Guyutongxue <guyutongxue@163.com>
2022-09-19 22:17:05 +0800

    update answers

--- docs/public/cpp/ca49691ea7eb41748b2d3f6f138aaff1.cpp
+++ docs/public/cpp/ca49691ea7eb41748b2d3f6f138aaff1.cpp
@@ -5,7 +5,7 @@ constexpr const double PRICES[]{28.9, 32.7, 45.6, 78.0, 35.0, 86.2, 27.8, 43.0, 
 int main() {
     int k;
     std::cin >> k;
-    std::cout.setf(std::ios::fixed);
+    std::cout << std::fixed;
     std::cout.precision(2);
     while (k--) {
         double sum{0.0};
--- docs/public/ica/ca49691ea7eb41748b2d3f6f138aaff1.cpp
+++ docs/public/ica/ca49691ea7eb41748b2d3f6f138aaff1.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    const double PRICES[] = {28.9, 32.7, 45.6, 78.0, 35.0, 86.2, 27.8, 43.0, 56.0, 65.0};
+    constexpr double PRICES[] = {28.9, 32.7, 45.6, 78.0, 35.0, 86.2, 27.8, 43.0, 56.0, 65.0};
     int k;
     cin >> k;
-    cout.setf(ios::fixed);
+    cout << fixed;
     cout.precision(2);
     for (int i = 0; i < k; i++) {
         double sum{0.0};
