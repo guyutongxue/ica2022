216dd2ced85115efac21359317c27f352a691fb1
Guyutongxue <guyutongxue@163.com>
2022-10-02 13:51:35 +0800

    fixes

--- docs/public/ica/82731edfbf81434e8750d7ad20b66359.cpp
+++ docs/public/ica/82731edfbf81434e8750d7ad20b66359.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int digitSum[10001] = {};
-    int m = 0, n = 0, k = 0;
+    int m, n, k;
     char c1, c2;
     cin >> m >> c1 >> n >> c2 >> k;
-
-    for (int i = m + 1; i < n; i++) {
-        int tmp = i;
-        while (tmp > 0) {
-            digitSum[i] += tmp % 10;
-            tmp /= 10;
-        }
-    }
-
-    int result[37][10001] = {};
-    int resultLen[37] = {};
-    for (int i = m + 1; i < n; i++) {
-        int sum = digitSum[i];
-        result[sum][resultLen[sum]++] = i;
-    }
-
-    for (int i = 1; i * k <= 36; i++) {
-        if (resultLen[i * k] != 0) {
-            cout << result[i * k][resultLen[0]];
-            for (int j = 1; j < resultLen[i * k]; j++) {
-                cout << "," << result[i * k][j];
+    for (int target = k; target <= 36; target += k) {
+        bool isFirst = true;
+        for (int i = m + 1; i < n; i++) {
+            int temp = i;
+            int sum = 0;
+            while (temp != 0) {
+                sum += temp % 10;
+                temp /= 10;
             }
+            if (sum == target) {
+                if (isFirst) {
+                    cout << i;
+                    isFirst = false;
+                } else {
+                    cout << ',' << i;
+                }
+            }
+        }
+        if (!isFirst) {
             cout << endl;
         }
     }
